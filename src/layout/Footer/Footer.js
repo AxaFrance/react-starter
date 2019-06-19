6b67b6182d829984996c64eed7bddc67a3348e69
Johnathan MEUNIER <jonathan.meunier@axa.fr>
2019-06-19 09:07:01 +0200

    Feat(Footer): affichage de la version npm dans le footer

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import logo from '@axa-fr/react-toolkit-core/dist/assets/logo-axa.svg';
-import { Footer } from '@axa-fr/react-toolkit-all';
-
+import { version } from '../../../package.json';
 import './Footer.scss';
 
-const FooterApp = () => {
-  return <Footer icon={logo} />;
-};
+const FooterApp = () => (
+  <footer className="footer">
+    <div className="container-fluid container">
+      <div className="af-logo">
+        <img className="af-logo__brand" src={logo} alt="Logo Axa" />
+      </div>
+      <div className="af-footer-content">
+        <div className="footer__content">
+          <div className="footer__base">
+            Titre du footer 
+            <i>Tous droits réservés</i>
+          </div>
+          <div className="footer__info">
+            <p>Version: {version}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+);
 
 export default FooterApp;
