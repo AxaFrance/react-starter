fd03c27ed00f63969772de2703ca5faae4db51c5
Johnathan MEUNIER <jonathan.meunier@axa.fr>
2019-10-03 15:45:23 +0200

    refactor(Home): delete useless fragment

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,31 +4,28 @@ import PropTypes from 'prop-types';
 import './Home.scss';
 
 export const Home = ({ location, title, subtitle }) => (
-  <>
-    <div className="home container">
-      <h1 className="af-title--content">Accueil</h1>
-      <p>
-        Donec sollicitudin molestie malesuada. Donec sollicitudin molestie
-        malesuada. Donec rutrum congue leo eget malesuada. Vivamus suscipit
-        tortor eget felis porttitor volutpat. Sed porttitor lectus nibh.
-        Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus
-        magna justo, lacinia eget consectetur sed, convallis at tellus. Proin
-        eget tortor risus. Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit. Pellentesque in ipsum id orci porta dapibus.
-      </p>
+  <div className="home container">
+    <h1 className="af-title--content">Accueil</h1>
+    <p>
+      Donec sollicitudin molestie malesuada. Donec sollicitudin molestie
+      malesuada. Donec rutrum congue leo eget malesuada. Vivamus suscipit tortor
+      eget felis porttitor volutpat. Sed porttitor lectus nibh. Curabitur arcu
+      erat, accumsan id imperdiet et, porttitor at sem. Vivamus magna justo,
+      lacinia eget consectetur sed, convallis at tellus. Proin eget tortor
+      risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Pellentesque in ipsum id orci porta dapibus.
+    </p>
 
-      <p>
-        Sed porttitor lectus nibh. Curabitur non nulla sit amet nisl tempus
-        convallis quis ac lectus. Mauris blandit aliquet elit, eget tincidunt
-        nibh pulvinar a. Sed porttitor lectus nibh. Quisque velit nisi, pretium
-        ut lacinia in, elementum id enim. Vivamus magna justo, lacinia eget
-        consectetur sed, convallis at tellus. Nulla quis lorem ut libero
-        malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit. Vivamus suscipit tortor eget felis porttitor volutpat. Sed
-        porttitor lectus nibh.
-      </p>
-    </div>
-  </>
+    <p>
+      Sed porttitor lectus nibh. Curabitur non nulla sit amet nisl tempus
+      convallis quis ac lectus. Mauris blandit aliquet elit, eget tincidunt nibh
+      pulvinar a. Sed porttitor lectus nibh. Quisque velit nisi, pretium ut
+      lacinia in, elementum id enim. Vivamus magna justo, lacinia eget
+      consectetur sed, convallis at tellus. Nulla quis lorem ut libero malesuada
+      feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+      suscipit tortor eget felis porttitor volutpat. Sed porttitor lectus nibh.
+    </p>
+  </div>
 );
 
 const propTypes = {
